227bdf5ebe8686825cd15d5100861521275fedd1
almeidadefaria <almeidadefaria@hotmail.com>
2021-05-24 16:54:31 -0300

    add slide

--- src/pages/AdPage/styled.js
+++ src/pages/AdPage/styled.js
@@ -24,19 +24,44 @@ export const PageArea = styled.div`
         flex: 1;
         margin-right: 20px;
 
-        .adImage{
-
+        .box{
+            display: flex;
         }
 
-        .adInfo{
-            padding: 10px;
+        .adImage{
+            width: 320px;
+            height: 320px;
+            margin-right: 20px;
+
+            .each-slide img{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-size: cover;
+                height: 320px;
+            }
+        }
+
+        .adInfo{  
+            flex: 1;
 
             .adName{
                 margin-bottom: 20px;
+
+                h2{
+                    margin: 0;
+                    margin-top: 20px;
+                }
+
+                small{
+                    color: #999;
+                }
             }
 
             .adDescription{
-
+                small{
+                    color: #999;
+                }
             }
         }
     }
